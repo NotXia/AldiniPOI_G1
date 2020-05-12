1e75c55bc29c023a2df180f3f55135a766b1db20
DarkCodes777 <39138636+darkcodes777@users.noreply.github.com>
2020-05-13 01:37:44 +0200

    Update login.php
    Verificare il corretto funzionamento con il db "reale"

--- login.php
+++ login.php
@@ -11,33 +11,43 @@
    require_once (dirname(__FILE__)."/util/token_gen.php");
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <title>Login</title>
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-   <link rel="stylesheet" href="./css/form_table.css">
-</head>
-   <body>
+<link rel="stylesheet" type="text/css" href="style.css">
+<!-- Required meta tags -->
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-      <div align="center">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+    <nav class="navbar navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">Aldini Valeriani</a>
+  
+  </div>
+</nav>
+    <title>Aldini Valeriani</title>
+<section id="cover" class="min-vh-100">
+    <div id="cover-caption">
+        <div class="container">
+            <div class="row text-white">
+                <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
+                    <h1 class="display-4 py-2 text-truncate">Login</h1>
+                    <p class="lead">Effettua il login con i tuoi dati.</p>
 
-         <h2>Login</h2>
 
-         <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
-            <table>
-               <tr>
-                  <td id="label">Email</td>
-                  <td id="padding"><input type="email" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" required></td>
-               </tr>
-               <tr>
-                  <td id="label">Password</td>
-                  <td id="padding"><input id="password" type="password" name="password" required></td>
-               </tr>
-            </table>
+                    <div class="px-2">
+                    <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <p>Email</p>
+                     <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" required></td>
+                     <br>
+                     <br>
+                     <p>Password</p>
+                     <input id="password" type="password" name="password" required>
+                     <br>
+
+
             <input type="checkbox" name="rememberme" value=""> Ricordami
             <br><br>
             <input type="submit" name="submit" value="Accedi">
@@ -45,9 +55,10 @@
          </form>
          <a href="./reset_password/request.php">Ho dimenticato la password</a>
          <br>
-      </div>
-   </body>
-</html>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 <?php
