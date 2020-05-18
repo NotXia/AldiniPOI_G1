2e4881adffa590c9811951c9f418f231c6753e59
hernylee <hernylee01@users.noreply.github.com>
2020-05-18 02:27:04 +0200

    Fix generali - Mappa

--- js/map.js
+++ js/map.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+    //------------------------------------------------------------------------------
+    //LISTENERS
+
     $("#_228").hover(function(){
         rectInHover("228");
         obscurePiano();
@@ -7,6 +11,13 @@ $(document).ready(function(){
         lightPiano();
     });
     $("#s228").hover(function(){
+        rectInHover("228");
+        obscurePiano();
+        }, function(){
+        rectUnHover("228");
+        lightPiano();
+    });
+    $("#t228").hover(function(){
         rectInHover("228");
         obscurePiano();
         }, function(){
@@ -28,6 +39,13 @@ $(document).ready(function(){
         rectUnHover("221");
         lightPiano();
       });
+    $("#t221").hover(function(){
+        rectInHover("221");
+        obscurePiano();
+        }, function(){
+        rectUnHover("221");
+        lightPiano();
+      });
 
     $("#_230").hover(function(){
         rectInHover("230");
@@ -43,7 +61,102 @@ $(document).ready(function(){
         rectUnHover("230");
         lightPiano();
       });
+    $("#t230").hover(function(){
+        rectInHover("230");
+        obscurePiano();
+        }, function(){
+        rectUnHover("230");
+        lightPiano();
+      });
     
+    $("#_288").hover(function(){
+        rectInHover("288");
+        obscurePiano();
+        }, function(){
+        rectUnHover("288");
+        lightPiano();
+      });
+    $("#s288").hover(function(){
+        rectInHover("288");
+        obscurePiano();
+        }, function(){
+        rectUnHover("288");
+        lightPiano();
+      });
+    $("#t288").hover(function(){
+        rectInHover("288");
+        obscurePiano();
+        }, function(){
+        rectUnHover("288");
+        lightPiano();
+      });
+
+    $("#_290").hover(function(){
+        rectInHover("290");
+        obscurePiano();
+        }, function(){
+        rectUnHover("290");
+        lightPiano();
+      });
+    $("#s290").hover(function(){
+        rectInHover("290");
+        obscurePiano();
+        }, function(){
+        rectUnHover("290");
+        lightPiano();
+      });
+    $("#t290").hover(function(){
+        rectInHover("290");
+        obscurePiano();
+        }, function(){
+        rectUnHover("290");
+        lightPiano();
+      });
+
+    $("#_292").hover(function(){
+        rectInHover("292");
+        obscurePiano();
+        }, function(){
+        rectUnHover("292");
+        lightPiano();
+      });
+    $("#s292").hover(function(){
+        rectInHover("292");
+        obscurePiano();
+        }, function(){
+        rectUnHover("292");
+        lightPiano();
+      });
+    $("#t292").hover(function(){
+        rectInHover("292");
+        obscurePiano();
+        }, function(){
+        rectUnHover("292");
+        lightPiano();
+      });
+
+    $("#_297b").hover(function(){
+        rectInHover("297b");
+        obscurePiano();
+        }, function(){
+        rectUnHover("297b");
+        lightPiano();
+      });
+    $("#s297b").hover(function(){
+        rectInHover("297b");
+        obscurePiano();
+        }, function(){
+        rectUnHover("297b");
+        lightPiano();
+      });
+    $("#t297b").hover(function(){
+        rectInHover("297b");
+        obscurePiano();
+        }, function(){
+        rectUnHover("297b");
+        lightPiano();
+      });
+
     $("#labs").hover(function(){
         rectInHover("221");
         rectInHover("228");
@@ -55,6 +168,42 @@ $(document).ready(function(){
         rectUnHover("230");
         lightPiano();
       });
+
+    $("#labsb").hover(function(){
+        rectInHover("288");
+        rectInHover("290");
+        rectInHover("292");
+        rectInHover("297b");
+        obscurePiano();
+        }, function(){
+        rectUnHover("288");    
+        rectUnHover("290");
+        rectUnHover("292");
+        rectUnHover("297b");
+        lightPiano();
+      });
+
+    $("#dipartimento").hover(function(){
+        rectInHover("221");
+        rectInHover("228");
+        rectInHover("230");
+        rectInHover("288");
+        rectInHover("290");
+        rectInHover("292");
+        rectInHover("297b");
+        obscurePiano();
+        }, function(){
+        rectUnHover("221");    
+        rectUnHover("228");
+        rectUnHover("230");
+        rectUnHover("288");    
+        rectUnHover("290");
+        rectUnHover("292");
+        rectUnHover("297b");
+        lightPiano();
+      });
+
+    //------------------------------------------------------------------------------
     
     function rectInHover(id) {
         rect_id = "#_" + id;
@@ -84,9 +233,12 @@ $(document).ready(function(){
 
     function obscurePiano() {
         $("#Piano").css("transition", "all 0.8s ease");
-        $("#Piano").css("opacity", "0.3");
+        $("#Piano").css("opacity", "0.2");
+        $("#Livello_2").css("transition", "all 0.8s ease");
+        $("#Livello_2").css("opacity", "0.2");
     }
     function lightPiano() {
         $("#Piano").css("opacity", "1");
+        $("#Livello_2").css("opacity", "1");
     }
   });
