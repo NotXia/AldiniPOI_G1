8cf9bf77dea8680cc1804224491ca9c7dc56b789
Xia <tiancheng.xia2001@gmail.com>
2020-05-18 21:24:00 +0200

    Pagina laboratorio

---
+++ view.php
@@ -0,0 +1,155 @@
+<?php
+   session_start();
+   require_once (dirname(__FILE__)."/util/dbconnect.php");
+   require_once (dirname(__FILE__)."/util/config.php");
+
+   $cod_permesso = 1;
+   if(isset($_SESSION["cod_permesso"])) {
+      if($_SESSION["cod_permesso"] == 3) {
+         $cod_permesso = 2;
+      }
+      else {
+         $cod_permesso = $_SESSION["cod_permesso"];
+      }
+   }
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+      <link rel="stylesheet" href="css/navbar.css">
+
+      <title>Prenotazioni</title>
+
+   </head>
+
+   <body>
+
+      <nav class="navbar navbar-dark bg-primary">
+         <div class="navbar-brand">
+            <a class="navbar-brand nav_nopadding" href="index.php">
+               <img class="nav_logo" src="res/logo.png" alt="AV Logo">
+               Aldini Valeriani
+            </a>
+         </div>
+         <div align="right">
+            <a class="nav_options" href="./map">Indietro</a>
+         </div>
+      </nav>
+
+      <section id="cover" class="min-vh-100">
+          <div id="cover-caption">
+              <div class="container">
+                  <div class="row text-black">
+                      <div class="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center p-4">
+                        <?php
+                           if(empty($_GET["tag"])) {
+                              die("<h3>Si è verificato un errore</h3>");
+                           }
+                           else {
+                              try {
+                                 $conn = db_connect();
+
+                                 // Estrae i dati del laboratorio
+                                 $sql = "SELECT *
+                                         FROM laboratori
+                                         WHERE tag = :tag";
+                                 $stmt = $conn->prepare($sql);
+                                 $stmt->bindParam(":tag", $_GET["tag"], PDO::PARAM_STR, 20);
+                                 $stmt->execute();
+                                 $res_lab = $stmt->fetch();
+                                 if(empty($res_lab)) {
+                                    die("<h3>Si è verificato un errore</h3>");
+                                 }
+                              } catch (PDOException $e) {
+                                 die("<h3>Si è verificato un errore</h3>");
+                              }
+                           }
+                        ?>
+                        <h1 class="display-4 py-2 text-truncate"><?php echo htmlentities($res_lab["nome"]); ?></h1>
+                        <p class="lead"><?php if(!empty($res_lab["descrizione"])) echo htmlentities($res_lab["descrizione"]); ?></p>
+                        <p class="lead" style="margin: 0 0 0;"><?php echo "Piano n°".htmlentities($res_lab["piano"]); ?></p>
+                        <p class="lead" style="margin: 0 0 0;"><?php if(!empty($res_lab["num_posti"])) echo htmlentities($res_lab["num_posti"])." postazioni"; ?></p>
+                        <p class="lead" style="margin: 0 0 0;"><?php if(!empty($res_lab["presenza_lim"])) echo htmlentities($res_lab["presenza_lim"]==1 ? "LIM" : ""); ?></p>
+                      </div>
+                   </div>
+
+                   <?php
+                      try {
+                        // Estrae le immagini del laboratorio
+                        $sql = "SELECT *
+                                FROM immagini
+                                WHERE cod_laboratorio = :tag AND
+                                      cod_permesso = $cod_permesso";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->bindParam(":tag", $_GET["tag"], PDO::PARAM_STR, 20);
+                        $stmt->execute();
+                        $res_img = $stmt->fetchAll();
+                      } catch (PDOException $e) {
+                         die("<h3>Si è verificato un errore</h3>");
+                      }
+                   ?>
+
+                   <div class="row">
+                      <div class="col-xl-8 col-lg-8 col-md-10 col-sm-12 mx-auto text-center">
+
+                        <div id="labImages" class="carousel slide" data-ride="carousel">
+                           <ol class="carousel-indicators">
+                              <?php
+                                 $i = 0;
+                                 foreach($res_img as $row) {
+                                    if($i==0)
+                                       echo "<li data-target='#labImages' data-slide-to='0' class='active'></li>";
+                                    else
+                                       echo "<li data-target='#labImages' data-slide-to='$i'></li>";
+                                    $i++;
+                                 }
+                              ?>
+                           </ol>
+                           <div class="carousel-inner">
+                              <?php
+                                 $i = 0;
+                                 foreach($res_img as $row) {
+                                    $path = $IMAGES_PATH . "/" . $row["percorso"];
+                                    $descrizione = $row["descrizione"];
+                                    if($i==0)
+                                       echo "<div class='carousel-item active' align='center'>";
+                                    else
+                                       echo "<div class='carousel-item' align='center'>";
+                                    echo "<img style='width:100%;' src='$path'>
+                                          <div class='carousel-caption'>
+                                             <div style='background-color:rgba(150, 150, 150, 0.6);padding:0'>
+                                                <p>$descrizione</p>
+                                             </div>
+                                          </div>
+                                       </div>";
+                                    $i++;
+                                 }
+                              ?>
+                           </div>
+                           <a class="carousel-control-prev" href="#labImages" role="button" data-slide="prev">
+                              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Indietro</span>
+                           </a>
+                           <a class="carousel-control-next" href="#labImages" role="button" data-slide="next">
+                              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                              <span class="sr-only">Avanti</span>
+                           </a>
+                        </div>
+
+                     </div>
+                   </div>
+               </div>
+           </div>
+      </section>
+
+   </body>
+</html>
